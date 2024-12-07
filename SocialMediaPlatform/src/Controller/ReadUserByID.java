59c6cb0bf77de8c18f473f75cc4d1c9618a13d63
osamahagr <osamahagr96@gmail.com>
2024-12-07 03:25:29 +0300

    add post Services

--- SocialMediaPlatform/src/Controller/ReadUserByID.java
+++ SocialMediaPlatform/src/Controller/ReadUserByID.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
-/**
- *
- * @author Al-Mohtareef
- */
-public class NewClass {
-    
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Model.Database;
+import Model.User;
+import View.Alert;
+
+
+public class ReadUserByID {
+    private User user;
+
+    public ReadUserByID(String userId, Database database) {
+        try {
+            database.findUserById(userId).ifPresent(userJson -> {
+                user = new User();
+                user.setUserID(userJson.getString("userId"));
+                user.setFirstName(userJson.getString("firstName"));
+                user.setLastName(userJson.getString("lastName"));
+                user.setEmail(userJson.getString("email"));
+            });
+        } catch (Exception e) {
+            new Alert("Error reading user by ID: " + e.getMessage(), null);
+        }
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
+
